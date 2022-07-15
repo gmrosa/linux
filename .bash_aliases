cec13c49c62793966170554862bfca035970abcb
Guilherme Murilo da Rosa <gmurilorosa@gmail.com>
2022-07-15 14:23:56 -0300

    Install ubuntu

--- .bash_aliases
+++ .bash_aliases
@@ -1,35 +1,78 @@
+# colours
+RED_PATTERN='(?:\d+\/\d+\s+(?!(?:Running)|(?:Completed))\w+\s+\d+ )'
+YELLOW_PATTERN='(?:(\d+)\/\1\s+Running\s+(?:(?:[1-9]{1} )|(?:[0-9]{2,} )))'
+GREEN_PATTERN='(?:([0-9]+)\/\1\s+Running\s+0)|(?:0\/[0-9]+\s+Completed\s+[0-9]+)'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+BLUE='\033[01;34m'
+NONE='\033[0m'
 # default
 alias sudo='sudo '
+alias ls='ls --color'
+alias ll='ls -lh'
+alias grep="grep --color"
+alias ..="cd .."
+alias ...="cd ../../"
+alias ip="/usr/sbin/ip -c"
 # bash
-alias bashrcEdit='sudo code ~/.bashrc'
-alias bashrcRefresh='source ~/.bashrc'
-alias bashAliasEdit='sudo code ~/.bash_aliases'
-alias bashAliasRefresh='source ~/.bash_aliases'
+alias bashrcedit='code ~/.bashrc'
+alias sourcebashrc='source ~/.bashrc'
+alias bashaliasedit='code ~/.bash_aliases'
+alias sourcebashalias='source ~/.bash_aliases'
 # dev
-alias cdDev='cd ~/dev'
+alias cddev='cd ~/dev'
 # git
-alias gitAddAll='git ls-files --modified | xargs git add'
-alias gitCheckout='_gitCheckout(){ echo "git checkout --track origin/$1" ; git checkout --track origin/$1; }; _gitCheckout'
-alias gitClean='gitRefresh; gitPrune'
-alias gitDeleteLocalBranch='_gitDeleteLocalBranch(){ echo "git branch -d $1"; git branch -d $1;}; _gitDeleteLocalBranch'
-alias gitDeleteRemoteBranch='git push -d origin $1'
-alias gitListLocalBranches='git branch -l'
-alias gitListRemoteBranches='git branch -r'
-alias gitPrune='git remote prune origin'
-alias gitRefresh='_gitRefresh(){ git fetch; git pull; }; _gitRefresh'
-alias gitRename='_gitRename(){ echo "git branch -m $1" ; git branch -m $1; }; _gitRename'
+alias gitaddall='git add -A'
+alias gitcommit='_gitcommit(){ echo "git commit -m $1" ; git commit -m $1; }; _gitcommit'
+alias gitpush='_gitpush(){ echo "git push origin $1" ; git push origin $1; }; _gitpush'
+alias gitcheckout='_gitcheckout(){ echo "git checkout --track origin/$1" ; git checkout --track origin/$1; }; _gitcheckout'
+alias gitclean='gitrefresh; gitprune'
+alias gitdeletelocalbranch='_gitdeletelocalbranch(){ echo "git branch -d $1"; git branch -d $1;}; _gitdeletelocalbranch'
+alias gitdeleteremotebranch='git push -d origin $1'
+alias gitlistlocalbranches='git branch -l'
+alias gitlistremotebranches='git branch -r'
+alias gitprune='git remote prune origin'
+alias gitrefresh='_gitrefresh(){ git fetch; git pull; }; _gitrefresh'
+alias gitrename='_gitrename(){ echo "git branch -m $1" ; git branch -m $1; }; _gitrename'
 # giteye
-alias gitEye='~/dev/giteye/./GitEye &'
+alias giteye='~/dev/giteye/./GitEye &'
 # gradlew
-alias gradleBootRun='_bootRun(){ ./gradlew bootRun --args="--spring.profiles.active=$1";}; _bootRun'
+alias gradlebootrun='_bootRun(){ ./gradlew bootRun --args="--spring.profiles.active=$1";}; _bootRun'
 alias java18='sdk use java open-jdk-18'
 alias java11='sdk use java open-jdk-11'
 alias java8='sdk use java oracle-8'
 # kubernetes
-alias kubectx="kubectl ctx"
-alias kubens="kubectl ns"
-alias k8sDeletePod='_k8sDeletePod(){ echo "kubectl delete pod $1"; kubectl delete pod $1;}; _k8sDeletePod'
-alias k8sDeletePodForce='_k8sDeletePodForce(){ echo "kubectl delete pod $1 --grace-period=0 --force"; kubectl delete pod $1 --grace-period=0 --force;}; _k8sDeletePodForce'
-alias k8sGetLogs='_k8sGetLogs(){ echo "kubectl logs $1"; kubectl logs $1;}; _k8sGetLogs'
-alias k8sGetPods='kubectl get pods'
-alias k8sScaleZero='_k8sScaleZero(){ echo "kubectl scale --replicas=0 deployment/$1"; kubectl scale --replicas=0 deployment/$1;}; _k8sScaleZero'
+alias k="kubectl"
+alias kget="kubectl get"
+alias kpods='kubectl get pods'
+alias krc='kubectl resource-capacity --pods'
+alias kdp="kubectl describe pod"
+alias kdn="kubectl describe node"
+alias kdeployments="kubectl get deployments"
+alias kpvc="kubectl get pvc"
+alias kall="kubectl get all"
+alias ksvc="kubectl get svc"
+alias krs="kubectl get rs"
+alias kgetctx="kubectl config get-contexts"
+alias kctx="kubectl ctx"
+alias kns="kubectl ns"
+alias kdeletepod='_kdeletepod(){ echo "kubectl delete pod $1"; kubectl delete pod $1;}; _kdeletepod'
+alias kdeletepodforce='_kdeletepodforce(){ echo "kubectl delete pod $1 --grace-period=0 --force"; kubectl delete pod $1 --grace-period=0 --force;}; _kdeletepodforce'
+alias klogs='_klogs(){ echo "kubectl logs $1"; kubectl logs $1;}; _klogs'
+alias kscalezero='_kscalezero(){ echo "kubectl scale --replicas=0 deployment/$1"; kubectl scale --replicas=0 deployment/$1;}; _kscalezero'
+
+
+function kcountnotes(){
+	kubectl get nodes --no-headers | awk -v _date=$(date +%F-%T) -v y=$YELLOW -v r=$RED -v g=$GREEN -v b=$BLUE -v n=$NONE '{a[$2]++}END{ for (i in a)printf "%-1s %-1s %-1s %-1s %-1s \n", _date , y"Status:"n, g i n , y"Node Count"n  , g a[i] n;}'
+}
+function ksortpods(){
+	awk '!/STATUS/ { if( NF==6 || NF>=10 ) { print $4 } else if( NF==5 || NF==9 ) { print $3 } }' | sort | uniq -c | sort -nr | column -t
+}
+function kgetzones(){
+	kubectl get nodes -o json | jq -r '.items[] | "Node: " + .metadata.name + " Zone: "  + .metadata.labels."topology.kubernetes.io/zone"' | column -t
+}
+
+# audio
+alias hearmyvoice="pactl load-module module-loopback latency_msec=1"
+alias stophearingmyvoice="pactl unload-module module-loopback"
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -6,77 +6,141 @@ sudo apt-get -y upgrade
 echo "done!"
 
 echo " ... install ... "
-sudo apt install -y htop git curl flameshot unzip ca-certificates software-properties-common apt-transport-https wget
+sudo apt install -y htop default-jdk git curl zsh libreoffice-calc pactl flameshot pavucontrol net-tools alien unzip ca-certificates software-properties-common apt-transport-https wget
 mkdir ~/dev
 echo "install done!"
 
-echo " ... default-jdk ... "
-sudo apt install -y default-jdk
-echo "default-jdk done!"
+read -p "Install ohmyzsh?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo " ... ohmyzsh ... "
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo " ... ohmyzsh ... "  
+fi
 
-# https://www.how2shout.com/linux/how-to-install-giteye-gui-git-client-on-ubuntu-22-04-20-04-lts/
-echo " ... giteye ... "
-wget https://www.collab.net/sites/default/files/downloads/GitEye-2.2.0-linux.x86_64.zip
-mkdir ~/dev/giteye
-unzip GitEye-*-linux.x86_64.zip -d ~/dev/giteye
-sudo rm GitEye-*-linux.x86_64.zip
-echo "giteye done!"
+read -p "Install remmina?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo " ... remmina ... "
+  sudo snap install remmina --devmode
+  echo " ... remmina ... "
+fi
 
-# https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-ubuntu-20-04/
-echo " ... chrome ... "
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-echo "chrome done!"
+read -p "Install giteye?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # https://www.how2shout.com/linux/how-to-install-giteye-gui-git-client-on-ubuntu-22-04-20-04-lts/
+  echo " ... giteye ... "
+  wget https://www.collab.net/sites/default/files/downloads/GitEye-2.2.0-linux.x86_64.zip
+  mkdir ~/dev/giteye
+  unzip GitEye-*-linux.x86_64.zip -d ~/dev/giteye
+  sudo rm GitEye-*-linux.x86_64.zip
+  echo "giteye done!"
+fi
 
-# https://kimconnect.com/linux-how-to-install-visual-studio-code/
-echo " ... visual studio ... "
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt-get -y update
-sudo apt install -y code
-echo "visual studio done!"
+read -p "Install chrome?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-ubuntu-20-04/
+  echo " ... chrome ... "
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
+  echo "chrome done!"
+fi
 
-# https://linuxconfig.org/how-to-install-intellij-idea-on-ubuntu-20-04-linux-desktop
-echo " ... intellij ... "
-sudo snap install intellij-idea-community --classic
-echo "intellij done!"
+read -p "Install visual studio?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # https://kimconnect.com/linux-how-to-install-visual-studio-code/
+  echo " ... visual studio ... "
+  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+  sudo apt-get -y update
+  sudo apt install -y code
+  echo "visual studio done!"
+fi
 
-# https://docs.docker.com/engine/install/ubuntu/
-echo " ... docker ... "
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -aG docker $USER
-newgrp docker
-echo "docker done!"
+read -p "Install intellij?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # https://linuxconfig.org/how-to-install-intellij-idea-on-ubuntu-20-04-linux-desktop
+  echo " ... intellij ... "
+  sudo snap install intellij-idea-community --classic
+  echo "intellij done!"
+fi
 
-# https://docs.portainer.io/v/ce-2.9/start/install/server/docker/linux
-echo " ... portainer ... "
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name portainer \
-    --restart=always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-    portainer/portainer-ce .
-echo "portainer done!"
+read -p "Install docker?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # https://docs.docker.com/engine/install/ubuntu/
+  echo " ... docker ... "
+  sudo apt-get install \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get -y update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo usermod -aG docker $USER
+  newgrp docker
+  echo "docker done!"
+fi
 
-# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
-echo " ... kubernetes ... "
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-echo "kubernetes done!"
+read -p "Install aws?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo " ... aws ... "
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+    echo "aws done!"
+fi
 
+read -p "Install portainer?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # https://docs.portainer.io/v/ce-2.9/start/install/server/docker/linux
+  echo " ... portainer ... "
+  docker volume create portainer_data
+  docker run -d -p 8000:8000 -p 9000:9000 --name portainer \
+      --restart=always \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v portainer_data:/data \
+      portainer/portainer-ce .
+  echo "portainer done!"
+fi
+
+read -p "Install kubernetes?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+  echo " ... kubernetes ... "
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+  echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  echo "kubernetes done!"
+fi
+
+read -p "Install kubectx & kubens?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 # https://github.com/ahmetb/kubectx
 echo " ... kubectx & kubens ... "
 (
@@ -94,6 +158,10 @@ kubectl krew install ctx
 kubectl krew install ns
 echo "kubectx & kubens done!"
 
+read -p "Install command-line fuzzy finder?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 # https://github.com/junegunn/fzf
 echo " ... command-line fuzzy finder ... "
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -101,6 +169,10 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 source ~/.bashrc
 echo "command-line fuzzy finder done!"
 
+read -p "Install sdkman?" -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 # https://sdkman.io/install
 echo " ... sdkman ... "
 curl -s "https://get.sdkman.io" | bash
